2dacdb7170d6113ed4cf70adaa5b9d9d9e7f6c98
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2025-03-22 15:53:24 +1100

    [FEAT] Created home page and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} h-full antialiased`}
+        className={`${inter.className} h-full antialiased bg-[#fffae0]`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
