f4e789a78e06a74c6e89f46ad907db7346a771e3
TomoriNow <seanarcher1974@gmail.com>
2025-04-01 22:09:56 +1100

    finished navigation bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,6 +11,8 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
+  DrawerPortal,
+  DrawerOverlay
 } from "@/components/ui/drawer"
 
 const Navbar = () => {
@@ -21,24 +23,23 @@ const Navbar = () => {
       </div>
       <div>
         <div className='block sm:hidden'>
-
-          <Drawer direction='top'>
-            <DrawerTrigger>
-              <Menu />
-            </DrawerTrigger>
-            <DrawerContent className='top-0 h-56 fixed'>
-              <DrawerHeader>
-                <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-                <DrawerDescription>This action cannot be undone.</DrawerDescription>
-              </DrawerHeader>
-              <DrawerFooter>
-                <Button>Submit</Button>
-                <DrawerClose>
-                  <Button variant="outline">Cancel</Button>
-                </DrawerClose>
-              </DrawerFooter>
+        <Drawer direction="top">
+          <DrawerTrigger className="relative flex h-10 flex-shrink-0 items-center justify-center gap-2 overflow-hidden rounded-full px-4 text-sm font-medium shadow-sm transition-all hover:bg-[#FAFAFA] dark:bg-[#161615] dark:hover:bg-[#1A1A19] dark:text-white">
+            <Menu />
+          </DrawerTrigger>
+          <DrawerOverlay className="fixed inset-0 bg-black/40 transition-all duration-500 ease-in-out" />
+          <DrawerContent
+              className="m-0 left-7 right-7 top-0 bottom-50 outline-none w-[450px] rounded-b-2xl rounded-t-none bg-[#fffae0] transition-transform duration-500 ease-in-out overflow-visible"
+          >
+              <div className="p-8 flex flex-col justify-between items-center">
+                <div className="max-w-md mx-auto ">
+                    <Button variant={'link'} className='text-lg font-medium'>About Us</Button>
+                    <Button variant={'link'} className='text-lg font-medium'>Events</Button>
+                    <Button variant={'link'} className='text-lg font-medium'>Members</Button>
+                </div>
+              </div>
             </DrawerContent>
-          </Drawer>
+        </Drawer>
         </div>
         <div className='hidden sm:flex gap-3'>
           <Button variant={'link'} className='text-lg font-medium'>About Us</Button>
