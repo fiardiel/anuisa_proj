76ed6e3f263ffce0c86e113095ea84df91e7e901
TomoriNow <seanarcher1974@gmail.com>
2025-04-03 22:45:54 +1100

    Design fixes for navbar, added responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
           <p className="text-5xl font-semibold">
             Indonesian Students Association
           </p>
-          <p className="mt-5 w-3/5 text-2xl">
+          <p className="mt-10 w-3/5 text-2xl">
             Through this platform, ANUISA seeks to connect Indonesian talents within the ANU community to industry and beyond, while fostering employability and idea exchange.
           </p>
         </div>
@@ -22,14 +22,14 @@ export default async function Home() {
         </div>
         <div className='p-10 mt-10'>
         <div className="relative flex items-center justify-center w-full">
-          <hr className="w-full h-1 my-8 bg-gray-200 border-0 rounded-sm dark:bg-gray-700" />
+          <hr className="w-full h-1.5 my-8 bg-[#bf872b] border-0 rounded-sm dark:bg-gray-700" />
           <div className="absolute px-4 bg-white dark:bg-gray-900">
-              <h1 className="text-5xl font-semibold whitespace-nowrap">
-          Meet Indonesian Talents at ANU
-              </h1>
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold text-center whitespace-normal">
+              Meet Indonesian Talents at ANU
+            </h1>
           </div>
         </div>
-            <div>
+            <div className="relative flex items-center">
               <MemberList members={members} />
             </div>
           </div>
