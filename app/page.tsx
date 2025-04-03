4b344b2b2834ece1537ca8242f6b3565589ab683
TomoriNow <seanarcher1974@gmail.com>
2025-04-03 21:29:11 +1100

    added members section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import MemberList from "@/components/members/MemberList";
+import fetchMembers from "@/lib/members/fetchMembers";
+
+
+export default async function Home() {
+  const members = await fetchMembers()
   return (
     <div className="p-10">
       <div className="mt-20">
@@ -16,7 +21,13 @@ export default function Home() {
             About us
           </button>
         </div>
+        <div className='p-10 mt-10'>
+          <h1 className='text-5xl font-semibold'>Members</h1>
+            <div>
+              <MemberList members={members} />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
