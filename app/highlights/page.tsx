d660843b8f143cfed364ba979a1156dab602b64b
TomoriNow <seanarcher1974@gmail.com>
2025-04-07 16:04:30 +1000

    [FEAT] Added more photos to slider

--- app/highlights/page.tsx
+++ app/highlights/page.tsx
@@ -6,11 +6,20 @@ import Reveal from '../../components/utils/Reveal';
 import highlight1 from '@/public/highlight1.jpg';
 import highlight2 from '@/public/highlight2.jpg';
 import highlight3 from '@/public/highlight3.jpg';
+import highlight4 from '@/public/highlight4.jpg';
+import highlight5 from '@/public/highlight5.jpg';
+import highlight6 from '@/public/highlight6.jpg';
+import highlight7 from '@/public/highlight7.jpg';
 
 const highlights = [
   { src: highlight1 },
   { src: highlight2 },
   { src: highlight3 },
+  { src: highlight4 },
+  { src: highlight5 },
+  { src: highlight6 },
+  { src: highlight7 },
+  
 ];
 
 const Highlights = () => {
